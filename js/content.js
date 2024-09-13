7616205251de4405f6e4f67ea319d5a8cd8b52c7
MajortomVR <majortom.rb@gmail.com>
2024-09-13 18:50:22 +0200

    refactor: reorganize scrolling and button code into separate classes

--- js/content.js
+++ js/content.js
@@ -1,10 +1,149 @@
-let interval = null;
-let smoothInterval = null;
-let distanceRemaining = 0;
-let distance = 0;
-let lastTimestamp = 0;
 
-let settings = {...DEFAULT_SETTINGS};
+class AutoScroll {
+    static FRAMES_PER_SECOND = 60;
+
+    constructor() {
+        this.settings = {...DEFAULT_SETTINGS};
+        
+        this.interval = null;
+        this.smoothInterval = null;
+        this.speedPixelPerSecond = 0.0;
+        this.distanceRemaining = 0;
+        this.distance = 0;
+        this.lastTimestamp = 0;
+
+        this.listeners = [];
+    }
+
+    updateSettings(updatedSettings) {
+        this.settings = {...updatedSettings};
+
+        if (this.isActive()) {
+            this.start();
+        }
+    }
+
+    isActive() {
+        return this.interval != null;
+    }
+
+    addStateChangedListener(listener) {
+        this.listeners.push(listener);
+    }
+
+    start() {
+        this.stop();
+        this.interval = setInterval(() => this.#scrollDown(), Math.round(this.settings.interval * 1000));        
+        this.#emitEvent();        
+    }
+
+    stop() {
+        if (this.smoothInterval) {
+            clearInterval(this.smoothInterval);
+            this.smoothInterval = null;
+        }
+
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+        
+        this.#emitEvent();
+    }
+
+    #scrollDown() {
+        const height = window.innerHeight;
+        
+        const distanceToScroll = height * (this.settings.distance / 100.0);
+        this.speedPixelPerSecond = distanceToScroll / this.settings.speed;
+        this.distanceRemaining = distanceToScroll;
+        this.distance = 0;
+
+        if (!this.smoothInterval) {
+            this.smoothInterval = setInterval(() => this.#smoothScroll(), 1000 / AutoScroll.FRAMES_PER_SECOND);
+            this.lastTimestamp = performance.now();
+        }        
+    }
+
+    #smoothScroll() {
+        let deltaTime = (performance.now() - this.lastTimestamp) / 1000.0;
+        
+        this.distance += this.speedPixelPerSecond * deltaTime;        
+        const distanceMoved = Math.trunc(this.distance);
+
+        this.distance -= distanceMoved;
+        this.distanceRemaining -= distanceMoved;
+        
+        if (this.distanceRemaining <= 1) {
+            clearInterval(this.smoothInterval);
+            this.smoothInterval = null;
+        }
+        
+        window.scrollBy(0, distanceMoved);
+
+        if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+            console.log("Scrolling stopped -> end reached!");
+            this.stop();            
+        }
+
+        this.lastTimestamp = performance.now();
+    }
+
+    #emitEvent() {
+        this.listeners.forEach(listener => listener());
+    }
+}
+
+
+class Button {
+    constructor(autoScrollInstance) {
+        this.autoScrollInstance = autoScrollInstance;
+    }
+
+    get() {
+        return document.getElementById('button-autoscroll-jscroll');
+    }
+
+    create() {
+        // If we can't find the button try to create it
+        if (!this.get()) {
+            const button = document.createElement('button');
+
+            if (button) {
+                button.id = 'button-autoscroll-jscroll';
+                button.innerText = '';
+
+                document.body.appendChild(button);
+
+                this.autoScrollInstance.addStateChangedListener( this.updateText.bind(this) );
+                
+                button.addEventListener('click', () => {
+                    if (this.autoScrollInstance.isActive()) {
+                        this.autoScrollInstance.stop();
+                    } else {
+                        this.autoScrollInstance.start();
+                    }
+                });
+
+                this.updateText();
+            }
+        }        
+    }
+
+    updateText() {
+        const button = this.get();
+
+        if (button) {
+            button.innerText = this.autoScrollInstance.isActive() ? 'Stop Autoscroll' : 'Start Autoscroll';
+        }
+    }
+}
+
+
+
+
+const autoScroll = new AutoScroll();
+const button = new Button(autoScroll);
 
 
 window.onload = () => {
@@ -13,129 +152,23 @@ window.onload = () => {
     // Show the button after the extension icon was pressed.
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         if (message.action === 'showButton') {
-            if (!getButton()) {
-                createButton();
-            }
+            button.create();
         }
     });
 
     // Update Settings
-    chrome.storage.onChanged.addListener((changes) => {
-        loadSettings();
-    });
+    chrome.storage.onChanged.addListener((changes) => loadSettings());
 }
 
-// Creates a button to control autoscroll start/stop
-function createButton() {
-    const button = document.createElement('button');
-    button.id = 'button-autoscroll-jscroll';
-    button.innerText = '';    
-
-    document.body.appendChild(button);
-    
-    updateButtonText();
-
-    button.addEventListener('click', () => {
-        toggleScrolling();
-        updateButtonText();
-    });
-}
 
 // Loads the extensions settings
 function loadSettings() {
     moduleLoadSettings()
         .then(loadedSettings => {
-            settings = {...loadedSettings};
-            
-            if (getStateAutoScroll()) {
-                startScrolling();
-            }
+            autoScroll.updateSettings(loadedSettings);
         })
         .catch(error => {
             console.log('Error loading settings.');
+            autoScroll.updateSettings(...DEFAULT_SETTINGS);
         });
-}
-
-// Return the Start/Stop Autoscrolling button
-function getButton() {
-    return document.getElementById('button-autoscroll-jscroll');
-}
-
-// Update the button's text depending on the current state
-function updateButtonText() {
-    const button = getButton();
-
-    if (button) {
-        button.innerText = getStateAutoScroll() ? 'Stop Autoscroll' : 'Start Autoscroll';
-    }
-}
-
-// Returns the state of Auto Scrolling.
-function getStateAutoScroll() {
-    return (interval !== null);
-}
-
-// Toggles Auto Scrolling ON/OFF
-function toggleScrolling() {    
-    if (interval) {
-        stopScrolling();
-    } else {
-        startScrolling();
-    }
-}
-
-// Call to start Auto Scrolling.
-function startScrolling() {
-    stopScrolling();
-    interval = setInterval(scrollDown, Math.round(settings.interval * 1000));
-    updateButtonText();
-}
-
-// Stops Auto Scrolling.
-function stopScrolling() {
-    if (smoothInterval) {
-        clearInterval(smoothInterval);
-        smoothInterval = null;
-    }
-    if (interval) {
-        clearInterval(interval);
-        interval = null;
-    }    
-    
-    updateButtonText();
-}
-
-// Called in intervals to scroll down smoothly.
-function scrollDown() {
-    const height = window.innerHeight;
-    distanceRemaining = height * (settings.distance / 100.0);
-    distance = 0;
-    
-    if (!smoothInterval) {
-        smoothInterval = setInterval(smoothScroll, 1000 / 30);
-        lastTimestamp = Date.now();
-    } 
-}
-
-// Called for a smooth scrolling animation.
-function smoothScroll() {
-    let deltaTime = (Date.now() - lastTimestamp) / 1000.0;
-    deltaTime = Math.min(0.1, deltaTime);
-
-    distance += distanceRemaining * (1.0 / settings.speed) * deltaTime;
-    distanceRemaining -= distance;
-
-    if (distanceRemaining <= 1) {
-        clearInterval(smoothInterval);
-        smoothInterval = null;
-    }
-
-    window.scrollBy(0, distance);
-
-    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
-        console.log("Scrolling stopped -> end reached!");
-        stopScrolling();
-    }
-
-    lastTimestamp = Date.now();
 }
